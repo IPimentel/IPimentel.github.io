82c8ac3c3596b3f1fbacc18b6fbe4f9a7846f974
IPimentel <ivan.jair.pimentel@gmail.com>
2018-10-29 21:20:30 -0600

    Update-29-Oct

---
+++ semana_8/dia_1/Ejercicio/instrucciones.js
@@ -0,0 +1,35 @@
+/*
+
+  El objetivo del día de hoy será terminar 
+  la función que muestra las tarjetas de la
+  respuesta que nos dá el API.
+
+  Una vez que termines esa parte deberás
+  implementar la funcionalidad para cambiar
+  de páginas, los URLs a las diferentes
+  páginas te lo regresa el mismo API dentro
+  del objeto principal
+
+  Ejemplo de respuesta:
+
+  {
+    info: {
+      count: 493
+      next: "https://rickandmortyapi.com/api/character/?page=2"
+      pages: 25
+      prev: ""
+    },
+    results: [...]
+  }
+
+  Tips: 
+  
+  - Utilizar una funcion 
+  para hacer el llamado 
+  al API, le pasarás el
+  de la página a cargar
+  de manera dinámica.
+  
+  - Mostrar botones para
+  hacer el cambio de página
+*/
